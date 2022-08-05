b371c1a44723de21897fa4e4b17144f3c9bdf087
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-05 07:17:13 +0300

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { UserTagsModule } from './user_tags/user_tags.module';
+import { TagsModule } from './tags/tags.module';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadModels: true,
+    }),
+    UsersModule,
+    UserTagsModule,
+    TagsModule,
+  ],
+})
+export class AppModule {}
