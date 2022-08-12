6b2c16cbbe69f6e635d98a35cb353268b1174671
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-12 06:09:39 +0300

    add Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 8080;
-  const app = NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
 
-  (await app).listen(PORT, () =>
-    console.log(`Server started on port = ${PORT}`),
-  );
+  const config = new DocumentBuilder()
+    .setTitle('Тестовое задание для NodeJS разработчика')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .addTag('ARAOvsepyan <ara07.99@icloud.com>')
+    .build();
+  const document = SwaggerModule.createDocument(await app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
 bootstrap();
