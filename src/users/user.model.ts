7eda2cc630284974f46261f3ddf09fc3ac2102f1
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-12 06:10:28 +0300

    Create user.model.ts

---
+++ src/users/user.model.ts
@@ -0,0 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Model, Table, DataType } from 'sequelize-typescript';
+
+interface UserCretionAttributs {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCretionAttributs> {
+  @ApiProperty({
+    example: '5eff1450-19df-11ed-ae08-3fb844e47ea4',
+    description: 'Уникальный идентификатор',
+  })
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV1,
+    primaryKey: true,
+  })
+  uid: string;
+
+  @ApiProperty({
+    example: 'example@example.com',
+    description: 'Почтовый адрес',
+  })
+  @Column({ type: DataType.STRING(100), unique: true, allowNull: false })
+  email: string;
+
+  @ApiProperty({
+    example: 'password1234',
+    description: 'Пароль пользователя',
+  })
+  @Column({ type: DataType.STRING(100), allowNull: false })
+  password: string;
+
+  @ApiProperty({
+    example: 'ExampleNick',
+    description: 'Уникальный никнейм',
+  })
+  @Column({ type: DataType.STRING(30), unique: true, allowNull: false })
+  nickname: string;
+}
