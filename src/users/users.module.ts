c5e638a8317b5b35f1e857d4091786de8218917f
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-17 05:19:41 +0300

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { jwtConstants } from 'src/auth/constants';
 import { User } from './user.model';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import UsersService from './users.service';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
 })
 export class UsersModule {}
