e653b0a7df4f73e0ac0892f2112cf23b49c7a3c4
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-17 06:25:25 +0300

    Update tags.service.ts

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,15 +1,11 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/user.model';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { SearchTagDto } from './dto/search-tag.dto';
 import { Tag } from './tags.model';
+import { UpdateTagDto } from './dto/update-tag.dto';
 
 @Injectable()
 export class TagsService {
@@ -19,8 +15,82 @@ export class TagsService {
     private jwtService: JwtService,
   ) {}
 
-  async getAllTags(search: SearchTagDto): Promise<Tag[]> {
-    return this.tagRepository.findAll();
+  async getAllTags(search: SearchTagDto) {
+    const page = search.page || 1;
+    const pageSize = search.pageSize || 10;
+    const offset = page * pageSize - pageSize;
+
+    if (search.sortByOrder != undefined) {
+      const tags = await this.tagRepository.findAndCountAll({
+        attributes: ['name', 'sortOrder'],
+        include: [
+          {
+            model: User,
+            attributes: ['uid', 'nickname'],
+          },
+        ],
+        order: ['sortOrder'],
+        limit: pageSize,
+        offset: offset,
+      });
+      return {
+        data: tags.rows,
+        meta: { page, pageSize, quantity: tags.count },
+      };
+    }
+
+    if (search.sortByName != undefined) {
+      const tags = await this.tagRepository.findAndCountAll({
+        attributes: ['name', 'sortOrder'],
+        include: [
+          {
+            model: User,
+            attributes: ['nickname', 'uid'],
+          },
+        ],
+        order: ['name'],
+        limit: pageSize,
+        offset: offset,
+      });
+
+      return {
+        data: tags.rows,
+        meta: { page, pageSize, quantity: tags.count },
+      };
+    }
+
+    if (search.sortByName && search.sortByOrder) {
+      const tags = await Tag.findAndCountAll({
+        attributes: ['name', 'sortOrder'],
+        include: [
+          {
+            model: User,
+            attributes: ['nickname', 'uid'],
+          },
+        ],
+        order: ['name', 'sortOrder'],
+        limit: pageSize,
+        offset: offset,
+      });
+
+      return {
+        data: tags.rows,
+        meta: { page, pageSize, quantity: tags.count },
+      };
+    }
+
+    const tags = await Tag.findAndCountAll({
+      attributes: ['name', 'sortOrder'],
+      include: [
+        {
+          model: User,
+          attributes: ['nickname', 'uid'],
+        },
+      ],
+      limit: pageSize,
+      offset: offset,
+    });
+    return { data: tags.rows, meta: { page, pageSize, quantity: tags.count } };
   }
 
   async getTagById(id: number): Promise<Tag> {
@@ -61,11 +131,69 @@ export class TagsService {
     }
   }
 
-  async updateTagById(id: number, tag: CreateTagDto) {
-    return this.tagRepository.update(tag, { where: { id } });
+  async updateTagById(
+    id: number,
+    header: { authorization: string },
+    tag: UpdateTagDto,
+  ) {
+    const token = header.authorization.split(' ')[1];
+    const decoded = this.jwtService.verify(token);
+
+    const conditate_tag = await Tag.findOne({ where: { id } });
+    if (conditate_tag.creatorUId === decoded.uid) {
+      await Tag.update({ ...tag }, { where: { id } });
+
+      const updated_tag = await Tag.findOne({
+        attributes: ['name', 'sortOrder'],
+        include: [
+          {
+            model: User,
+            attributes: ['uid', 'nickname'],
+          },
+        ],
+        where: { id: id },
+      });
+
+      return updated_tag;
+    } else {
+      return new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          error: 'Нет прав на редактирование этого тега',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
   }
 
-  async deleteTagById(id: number) {
-    return this.tagRepository.destroy({ where: { id } });
+  async deleteTagById(id: number, header: { authorization: string }) {
+    try {
+      const token = header.authorization.split(' ')[1];
+      const decoded = this.jwtService.verify(token);
+
+      const tag = await this.tagRepository.findOne({ where: { id } });
+
+      if (tag.creatorUId === decoded.uid) {
+        await this.tagRepository.destroy({ where: { id } });
+
+        return;
+      } else {
+        return new HttpException(
+          {
+            status: HttpStatus.UNAUTHORIZED,
+            error: 'Нет прав на удаление этого тега',
+          },
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+    } catch (error) {
+      return new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Тег не найден',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
