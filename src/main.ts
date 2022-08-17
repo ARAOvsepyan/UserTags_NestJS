22945c7908d355b7bd2b8caabe51d0bb3776a1f7
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-17 05:19:56 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
 
   const config = new DocumentBuilder()
@@ -12,8 +14,9 @@ async function bootstrap() {
     .setDescription('Документация REST API')
     .setVersion('1.0.0')
     .addTag('ARAOvsepyan <ara07.99@icloud.com>')
+    .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(await app, config);
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
   app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
