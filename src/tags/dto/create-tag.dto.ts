ff06bcfcd925cbf1f47e7a5773ef6d595e8616e2
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-17 05:19:55 +0300

    Create create-tag.dto.ts

---
+++ src/tags/dto/create-tag.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTagDto {
+  @ApiProperty({
+    example: 'ExampleTagName',
+    description: 'Название тега',
+  })
+  readonly name: string;
+  @ApiProperty({
+    example: '1',
+    description: 'Ключ сортировки',
+  })
+  readonly sortOrder: number;
+  @ApiProperty({
+    example: '5eff1450-19df-11ed-ae08-3fb844e47ea4',
+    description: 'Идентификатор пользователя',
+  })
+  readonly creatorUid: string;
+}
