0ecefd1fb686e5c799754a6adc7a691c0d206647
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-17 05:19:25 +0300

    Create tags.model.ts

---
+++ src/tags/tags.model.ts
@@ -0,0 +1,56 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  Model,
+  Table,
+  DataType,
+  BelongsTo,
+  ForeignKey,
+} from 'sequelize-typescript';
+import { User } from 'src/users/user.model';
+
+interface TagCretionAttributs {
+  name: string;
+  sortOrder: number;
+  creatorUId: string;
+}
+
+@Table({ tableName: 'tags' })
+export class Tag extends Model<Tag, TagCretionAttributs> {
+  @ApiProperty({
+    example: '1',
+    description: 'Уникальный идентификатор',
+  })
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: string;
+
+  @ApiProperty({
+    example: 'ExampleTagName',
+    description: 'Название тега',
+  })
+  @Column({ type: DataType.STRING(40), unique: true, allowNull: false })
+  name: string;
+
+  @ApiProperty({
+    example: '1',
+    description: 'Ключ сортировки',
+    default: '0',
+  })
+  @Column({ type: DataType.INTEGER, defaultValue: 0 })
+  sortOrder: number;
+
+  @ApiProperty({
+    example: '5eff1450-19df-11ed-ae08-3fb844e47ea4',
+    description: 'Идентификатор пользователя',
+  })
+  @ForeignKey(() => User)
+  @Column({ type: DataType.UUID, allowNull: false })
+  creatorUId: string;
+
+  @BelongsTo(() => User)
+  creator: User;
+}
