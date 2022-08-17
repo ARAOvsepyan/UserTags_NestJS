c6774270ccb60035c2b89655747411d42e81ad93
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-17 05:19:32 +0300

    Update tags.module.ts

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { jwtConstants } from 'src/auth/constants';
+import { User } from 'src/users/user.model';
+import { TagsController } from './tags.controller';
+import { Tag } from './tags.model';
+import { TagsService } from './tags.service';
 
-@Module({})
+@Module({
+  controllers: [TagsController],
+  providers: [TagsService],
+  imports: [
+    SequelizeModule.forFeature([User, Tag]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+})
 export class TagsModule {}
