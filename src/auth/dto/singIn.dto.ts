a612abc280810739ec61c82f08307b9cde3bd732
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-18 04:59:19 +0300

    Update singIn.dto.ts

--- src/auth/dto/singIn.dto.ts
+++ src/auth/dto/singIn.dto.ts
@@ -1,30 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsUppercase,
-  Length,
-  IsLowercase,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class SingInDto {
   @ApiProperty({
     example: 'example@example.com',
     description: 'Почтовый адрес',
   })
-  @IsEmail()
+  @IsString({ message: 'Почтовый адрес должен быть строкой' })
+  @IsEmail({}, { message: 'Некорректный адрес электронной почты' })
   readonly email: string;
   @ApiProperty({
     example: 'password1234',
     description: 'Пароль пользователя',
   })
-  @IsNotEmpty()
-  @Length(8, 20)
+  @IsNotEmpty({ message: 'Пароль не может быть пустым' })
+  @Length(8, 20, { message: 'Длина пароля должна быть от 8 до 20 символов' })
   readonly password: string;
   @ApiProperty({
     example: 'ExampleNick',
     description: 'Уникальный никнейм',
   })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Никнейм не может быть пустым' })
+  @IsString({ message: 'Никнейм должен быть строкой' })
   readonly nickname: string;
 }
