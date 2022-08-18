74b6783f18581bc596f420ce4b1ac9ae1e00cf43
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-18 04:59:43 +0300

    Update user_tags.controller.ts

--- src/user_tags/user_tags.controller.ts
+++ src/user_tags/user_tags.controller.ts
@@ -1,7 +1,78 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AddTagDto } from './dto/add-tags.dto';
+import { UserTags } from './user_tags.model';
 import { UserTagsService } from './user_tags.service';
 
-@Controller('user-tags')
+@ApiBearerAuth()
+@ApiTags('Теги пользователя')
+@UseGuards(JwtAuthGuard)
+@Controller('user/tags')
 export class UserTagsController {
   constructor(private readonly userTagsSevice: UserTagsService) {}
+
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Berare JWT',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Add tags' })
+  @ApiResponse({ status: 200, type: UserTags, isArray: true })
+  @Post()
+  async addTag(
+    @Headers() headers: { authorization: string },
+    @Body() tags: AddTagDto,
+  ) {
+    return this.userTagsSevice.addTag(tags, headers);
+  }
+
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Berare JWT',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Delete tag' })
+  @ApiResponse({ status: 200 })
+  @Delete(':id')
+  async deleteTag(
+    @Headers() headers: { authorization: string },
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.userTagsSevice.deleteTag(headers, id);
+  }
+
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Berare JWT',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Get all tags' })
+  @ApiResponse({
+    status: 200,
+    type: UserTags,
+    isArray: true,
+    description: 'All tags',
+  })
+  @Get('/my')
+  async getTags(@Headers() headers: { authorization: string }) {
+    return this.userTagsSevice.getTags(headers);
+  }
+
 }
