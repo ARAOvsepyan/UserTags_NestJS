a57b1fa7e773f7edb47f7be5412a19b3b492f6af
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-18 04:59:30 +0300

    Update create-tag.dto.ts

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -6,14 +6,17 @@ export class CreateTagDto {
     description: 'Название тега',
   })
   readonly name: string;
+
   @ApiProperty({
     example: '1',
     description: 'Ключ сортировки',
   })
   readonly sortOrder: number;
+
   @ApiProperty({
     example: '5eff1450-19df-11ed-ae08-3fb844e47ea4',
     description: 'Идентификатор пользователя',
   })
   readonly creatorUid: string;
+  length: number;
 }
